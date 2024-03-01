435b1345db11c631925781d1873dff8d2b9368cc
rokandic <155958535+rokandic@users.noreply.github.com>
2024-03-01 10:17:18 +0100

    Solution using HTML, CSS, JavaScript

---
+++ README.md
@@ -0,0 +1,29 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+
+## Overview
+The goal of this practice project was to create a form and validate all the input fields when submitting the form by pressing the button. If any input field is invalid it will display which input was invalid. If all the inputs are in valid format, it will display a short message and reset the input fields.
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- [Live Site]()
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties 
+- Flexbox
+- Mobile-first workflow
+- JavaScript
---
+++ validateInputs.js
@@ -0,0 +1,46 @@
+let form = document.getElementsByTagName('form')[0];
+form.onsubmit = validateInputs;
+
+let inputs = document.getElementsByTagName('input');
+
+for( let input of inputs) {
+  input.onfocus = focusInput;
+};
+
+function focusInput(event) {
+  let input = event.target;
+  input.classList.remove('input-invalid');
+}
+
+function validateInputs(event) {
+  event.preventDefault();
+  
+  let everythingValid = true;
+
+  let inputs = document.getElementsByTagName('input');
+  for (let input of inputs) {
+    let inputValid = true;
+    if(input.id=="email") {
+      let email = input.value;
+      let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;  
+      let validEmail = regex.test(email);
+      if(!email || !validEmail) 
+        inputValid = false;        
+    }
+    else if(input.value.length<1) 
+      inputValid = false;
+
+    if (!inputValid) {
+      input.classList.add('input-invalid');
+      everythingValid = false;
+    }
+  }  
+  form.focus();
+
+  if (everythingValid) {
+    alert("All fields entered correctly.");
+    for( let input of inputs) {
+      input.value = "";
+    }
+  }
+}
